02d4ed10dcd14d1e42e19e1ca80402c02bcae2be
Paulo Torres <paulomacieltorresfilho@gmail.com>
2022-01-20 01:24:50 -0300

    parte do filme principal quase finalizada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-console.log(API_KEY);
 const mainMovie = document.getElementsByClassName('filme-principal')[0];
-const imagesPath = 'https://image.tmdb.org/t/p/w500';
+const imagesPath = 'https://image.tmdb.org/t/p/w1280';
 
 
 const loadMainMovie = async () => {
-    const data = await fetch(`https://api.themoviedb.org/3/movie/551?api_key=${API_KEY}&language=pt-BR`)
+    const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR`)
         .then((response) => response.json())
+        .then((json) => json.results[Math.floor(Math.random() * json.results.length)])
         .catch((err) => console.log('Um erro ocorreu!: ', err.message));
     const image = data['backdrop_path'];
-    
     console.log(data);
     console.log(image);
-    mainMovie.style.backgroundImage = `url(${imagesPath + image})`;
+    mainMovie.style.background = `linear-gradient(rgba(0, 0, 0, .40), rgba(0, 0, 0, .40)100%), url(${imagesPath + image})`;
+    mainMovie.style.backgroundSize = 'cover';
     return data;
 }
 
