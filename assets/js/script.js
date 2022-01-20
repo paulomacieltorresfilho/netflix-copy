bb56c6344a589cec027339471a92b436be7c05fd
Paulo Torres <paulomacieltorresfilho@gmail.com>
2022-01-20 00:10:50 -0300

    começado a utilizar a api do the movie database

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,18 @@
-
-
-const API_KEY = process.env.API_KEY;
-
 console.log(API_KEY);
+const mainMovie = document.getElementsByClassName('filme-principal')[0];
+const imagesPath = 'https://image.tmdb.org/t/p/w500';
+
 
 const loadMainMovie = async () => {
-    const data = await fetch()
+    const data = await fetch(`https://api.themoviedb.org/3/movie/551?api_key=${API_KEY}&language=pt-BR`)
+        .then((response) => response.json())
+        .catch((err) => console.log('Um erro ocorreu!: ', err.message));
+    const image = data['backdrop_path'];
+    
+    console.log(data);
+    console.log(image);
+    mainMovie.style.backgroundImage = `url(${imagesPath + image})`;
     return data;
 }
+
+loadMainMovie();
