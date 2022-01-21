4e170f2b06ece499b79695651f59e382e3dddf37
Paulo Torres <paulomacieltorresfilho@gmail.com>
2022-01-21 02:33:52 -0300

    alguns detalhes do carrossel ajustados

--- assets/js/owl/setup.js
+++ assets/js/owl/setup.js
@@ -2,18 +2,19 @@ $('.owl-carousel').owlCarousel({
     // center:true,
     loop:true,
     margin:15,
-    nav:true,
+    nav:false,
+    dots:false,
     responsive:{
         0:{
-            items:1.5
+            items:2.5
         },
-        600:{
+        750:{
             items:3.5
         },
-        800:{
+        1250:{
             items:4.5
         },
-        1250:{
+        1400:{
             items:6.5
         }
     }
