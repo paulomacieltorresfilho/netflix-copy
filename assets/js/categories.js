0ec521a4e88d2dc911c570e9ac79912eca748f57
Paulo Filho <paulomacieltorresfilho@gmail.com>
2022-01-21 16:33:51 -0300

    iniciada implementacao do em alta, com filmes e series

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -6,6 +6,32 @@ const getTopTvShows = async () => {
     return await getResultsList(`/discover/tv?api_key=${API_KEY}&language=pt-BR&sort_by=popularity.desc`);
 }
 
+const getTop = async () => {
+	const movies = await getTopMovies();
+	const tvShows = await getTopTvShows();
+	const topMedias = [];
+	let m, t;
+	m = t = 0;
+
+	console.log('Movies');
+	movies.forEach((item) => {
+		console.log(item.popularity);
+	});
+
+	console.log('Series');
+	tvShows.forEach((item) => {
+		console.log(item.popularity);
+	});
+
+	for (let i = 0; i < movies.length; i++) {
+		if (movies[m] >= tvShows[t]) topMedias.push(movies[m++]);
+		else topMedias.push(tvShows[t++]);
+	}
+
+	return topMedias;
+}
+
+
 const getMoviesByGenres = async (genres) => {
     const genresObj = await fetch(`${API_PATH}/genre/tv/list?api_key=${API_KEY}`)
         .then((response) => response.json())
@@ -18,7 +44,7 @@ const getMoviesByGenres = async (genres) => {
     })
     genresIds = genresIds.join(',');
 
-    return await getResultsList(`/discover/movie?api_key=${API_KEY}&with_genres=${genresIds}&language=pt-BR&sort_by=popularity.desc`);
+    return await getResultsList(`/discover/movie?api_key=${API_KEY}&with_genres=${genresIds}&language=pt-BR&sort_by=vote_average.desc&vote_count.gte=500&primary_release_date.gte=2000-01-01`);
 }
 
 const getTvShowsByGenres = async (genres) => {  
@@ -36,13 +62,15 @@ const getTvShowsByGenres = async (genres) => {
     return await getResultsList(`/discover/tv?api_key=${API_KEY}&with_genres=${genresIds}&language=pt-BR&sort_by=popularity.desc`);
 }
 
-const insertImgs = async (className, func) => {
-    const itemsList = document.querySelectorAll(`.carrossel-filmes.${className} .item img`);
-    const moviesList = await func();
-    console.log(itemsList);
+const insertImgs = async (className, func, arg) => {
+    const itemsList = document.querySelectorAll(`.carrossel.${className} .item img`);
+    const moviesList = await func(arg);
     itemsList.forEach((item, index) => {
         item.src = `${IMAGES_PATH}${POSTER_SIZE}${moviesList[index].poster_path}`
     })
 }
 
-insertImgs('em-alta', getTopMovies);
+insertImgs('em-alta', getTop);
+insertImgs('f-para-familia', getMoviesByGenres, ['Family']);
+
+
